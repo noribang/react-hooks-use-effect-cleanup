d7dd3167f48253cc74eecf15d8d3053379a59de8
hoonkim <hoonk9387@gmail.com>
2022-06-12 12:06:18 -0500

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,15 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    /* Pass to cleanup function. */
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    /* Return cleanup function. */
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
